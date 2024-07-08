1a7da4b1944c11e53c20e242c75c6755d08d36bf
almos05 <tenderboylive@gmail.com>
2024-07-08 22:15:15 +0300

    master: add l1, l2, elastic to LinearReg

--- LinearReg.py
+++ LinearReg.py
@@ -1,10 +1,13 @@
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Lasso
+from sklearn.metrics import r2_score
 
 
 class MyLineReg:
-    def __init__(self, n_iter=100, learning_rate=0.1, weights=None, metric=None):
+    def __init__(self, n_iter=100, learning_rate=0.1, weights=None, metric='mse', reg=None, l1_coef: float = 0,
+                 l2_coef: float = 0):
         self.n_iter = n_iter
         self.learning_rate = learning_rate
         self.weights = weights
@@ -17,6 +20,16 @@ class MyLineReg:
             'mape': lambda y, y_pred: 100 * (abs((y - y_pred) / y).mean()),
             'r2': lambda y, y_pred: 1 - (sum((y - y_pred) ** 2) / sum((y - y.mean()) ** 2))
         }
+        self.reg = reg
+        self.l1_coef = l1_coef
+        self.l2_coef = l2_coef
+
+        if not self.reg:
+            self.l1_coef, self.l2_coef = 0, 0
+        elif self.reg == 'l1':
+            self.l2_coef = 0
+        elif self.reg == 'l2':
+            self.l1_coef = 0
 
     def __str__(self):
         return f'MyLineReg class: n_iter={self.n_iter}, learning_rate={self.learning_rate}'
@@ -36,11 +49,12 @@ class MyLineReg:
             error = self.__metrics[self.metric](y, y_pred)
 
             if verbose and ((i + 1) % verbose == 0 or i == 0):
-                print(f'{i + 1} | loss: {error} | {self.metric}' if i != 0 else f'start | loss: {error} | {self.metric}')
+                print(
+                    f'{i + 1} | loss: {error} | {self.metric}' if i != 0 else f'start | loss: {error} | {self.metric}')
 
-            gradient = (2 / n) * (y_pred - y).dot(X)
+            gradient_loss = (2 / n) * (y_pred - y).dot(X)
 
-            self.weights -= self.learning_rate * gradient
+            self.weights -= self.learning_rate * (gradient_loss + self.l1_coef * np.sign(self.weights) + 2 * self.l2_coef * self.weights)
 
         if self.n_iter:
             y_pred = X.dot(self.weights).values
@@ -59,8 +73,9 @@ class MyLineReg:
         return self.__error
 
 
-cls = MyLineReg(metric='r2')
+cls = MyLineReg(metric='r2', reg='l2', l2_coef=0.123)
 sk_cls = LinearRegression()
+ls = Lasso(alpha=0.123)
 
 x = pd.DataFrame({
     '_X1': [1, 2, 3, 4],
@@ -73,8 +88,13 @@ x_test = pd.DataFrame({
 y = pd.Series([2.3, 3.2, 4.5, 5.7])
 
 cls.fit(x, y, verbose=50)
+ls.fit(x, y)
 sk_cls.fit(x, y)
 
-print(cls.predict(x_test))
-print(sk_cls.predict(x_test))
-print(cls.get_best_score())
+print('MyLinPred', cls.predict(x_test))
+print('sklearn:', sk_cls.predict(x_test))
+print('Lasso:', ls.predict(x_test))
+print()
+print('MyLinPred', cls.get_best_score())
+print('sklearn:', r2_score(y, sk_cls.predict(x)))
+print('Lasso:', r2_score(y, ls.predict(x)))
