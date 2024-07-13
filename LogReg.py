e6aa6b6ce622c2e9e13b185364f6ac1b7c3bb8c4
almos05 <tenderboylive@gmail.com>
2024-07-13 12:31:41 +0300

    master: release LogReg 2.0

--- LogReg.py
+++ LogReg.py
@@ -1,3 +1,4 @@
+import random
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
@@ -50,21 +51,36 @@ class MyLogReg(Metrics):
     def __init__(self,
                  n_iter: int = 10,
                  learning_rate=0.1,
-                 metric=None
+                 metric='accuracy',
+                 reg=None,
+                 l1_coef=0.1,
+                 l2_coef=0.1,
+                 sgd_sample=None,
+                 random_state=42
                  ):
         super().__init__()
         self._n_iter = n_iter
         self._learning_rate = learning_rate
         self.__weights = None
         self.metric = metric
+        self._reg = reg
+        self._l1_coef = l1_coef
+        self._l2_coef = l2_coef
+        self._sgd_sample = sgd_sample
+        self.__random_state = random_state
 
     def __str__(self):
         return f'MyLogReg class: n_iter={self._n_iter}, learning_rate={self._learning_rate}'
 
     def fit(self, X, y, verbose=False):
+        random.seed(self.__random_state)
         X = X.copy()
         y = y.copy()
         n = X.shape[0]
+
+        if isinstance(self._sgd_sample, float):
+            self._sgd_sample = int(self._sgd_sample * n)
+        self.__check_reg_attrs()
 
         X.insert(0, 'Bias', np.ones(n))
 
@@ -74,9 +90,16 @@ class MyLogReg(Metrics):
             z = np.dot(X, self.__weights)
             y_pred = expit(z)
 
-            gradient = 1 / n * (y_pred - y).dot(X)
+            if self._sgd_sample:
+                sample_rows_idx = random.sample(range(X.shape[0]), self._sgd_sample)
+                gradient = 1 / self._sgd_sample * (y_pred[sample_rows_idx] - y[sample_rows_idx]).dot(
+                    X.iloc[sample_rows_idx])
+            else:
+                gradient = 1 / n * (y_pred - y).dot(X)
 
-            self.__weights -= self._learning_rate * gradient
+            gradient += self._l1_coef * np.sign(self.__weights) + self._l2_coef * 2 * self.__weights
+
+            self.__reducing_the_weight(i, gradient)
 
             if self.metric == 'roc_auc':
                 self._score = self._metrics[self.metric](y, self.predict_proba(X.iloc[:, 1:]))
@@ -95,9 +118,7 @@ class MyLogReg(Metrics):
 
         z = np.dot(X, self.__weights)
 
-        self._y_proba = expit(z)
-
-        return self._y_proba
+        return expit(z)
 
     def get_coef(self):
         return self.__weights[1:]
@@ -110,6 +131,20 @@ class MyLogReg(Metrics):
     def get_best_score(self):
         return self._score
 
+    def __check_reg_attrs(self):
+        if not self._reg:
+            self._l1_coef, self._l2_coef = 0, 0
+        elif self._reg == 'l1':
+            self._l2_coef = 0
+        elif self._reg == 'l2':
+            self._l1_coef = 0
+
+    def __reducing_the_weight(self, i, gradient):
+        if isinstance(self._learning_rate, (int, float)):
+            self.__weights -= self._learning_rate * gradient
+        else:
+            self.__weights -= self._learning_rate(i + 1) * gradient
+
 
 X, y = make_classification(n_samples=1000, n_features=20, n_informative=2, n_redundant=10, random_state=42)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
@@ -119,7 +154,7 @@ sk_cls = LogisticRegression(max_iter=1000)
 sk_cls.fit(X_train, y_train)
 y_pred_sk = sk_cls.predict(X_train)
 
-my_cls = MyLogReg(n_iter=1000, metric='accuracy')
+my_cls = MyLogReg(n_iter=1000, metric='accuracy', reg='elastic', learning_rate=lambda iter: 0.5 * (0.85 ** iter), sgd_sample=0.1)
 my_cls.fit(X_train, y_train, verbose=20)
 y_pred = my_cls.predict(X_train)
 
